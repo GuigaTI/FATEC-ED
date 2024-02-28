d2d61a450d659abaf8d4a27f96091e8705207832
Guilherme <160168674+GuigaTI@users.noreply.github.com>
2024-02-28 17:23:45 -0300

    Add files via upload

--- Lista 2/Exerc7.cpp
+++ Lista 2/Exerc7.cpp
@@ -7,50 +7,50 @@ using namespace std;
 int main()
 {
     double vetA[7], vetB[7], vetRes[7];
-    string vetOp[7];
+    char vetOp[7];
     for (int i = 0; i < 7; i++)
     {
-        cout << "Digite o " << i + 1 << " do primeiro vetor: ";
+        cout << "Digite o " << i + 1 << "numero do primeiro vetor: ";
         cin >> vetA[i];
-        cout << "Digite o " << i + 1 << " do segundo vetor: ";
+        cout << "Digite o " << i + 1 << "numero do segundo vetor: ";
         cin >> vetB[i];
-        cout << "Digite a " << i + 1 << "operacao que sera realizada, sendo soma para SOMA,sub para SUBTRACAO,div para DIVISAO e mult para MULTIPLICACAO ";
+        cout << "Digite a " << i + 1 << "operacao que sera realizada,  +  -  /  * : ";
         cin >> vetOp[i];
     }
     for (int i = 0; i < 7; i++)
     {
-        if (vetOp[i] == "soma")
+        if (vetOp[i] == '+')
         {
             vetRes[i] = vetA[i] + vetB[i];
         }
-        else if (vetOp[i] == "sub")
+        else if (vetOp[i] == '-')
         {
             vetRes[i] = vetA[i] - vetB[i];
         }
-        else if (vetOp[i] == "div")
+        else if (vetOp[i] == '/')
         {
             vetRes[i] = vetA[i] / vetB[i];
         }
-        else if (vetOp[i] == "mult")
+        else if (vetOp[i] == '*')
         {
             vetRes[i] = vetA[i] * vetB[i];
         }
     }
     for (int i = 0; i < 7; i++)
     {
-        if (vetOp[i] == "soma")
+        if (vetOp[i] == '+')
         {
             cout << vetA[i] << " + " << vetB[i] << " = " << vetRes[i] << endl;
         }
-        else if (vetOp[i] == "sub")
+        else if (vetOp[i] == '-')
         {
             cout << vetA[i] << " - " << vetB[i] << " = " << vetRes[i] << endl;
         }
-        else if (vetOp[i] == "div")
+        else if (vetOp[i] == '/')
         {
             cout << vetA[i] << " / " << vetB[i] << " = " << vetRes[i] << endl;
         }
-        else if (vetOp[i] == "mult")
+        else if (vetOp[i] == '*')
         {
             cout << vetA[i] << " * " << vetB[i] << " = " << vetRes[i] << endl;
         }
